dcd2f100eb3d811b4f3521c3c4a7977bf1677cc8
Antonio Perez <tonyboyangie3@gmail.com>
2016-05-26 13:05:44 -0400

    Clean

--- Codebase/LegendsOfWar/Assets/oldAssets/scripts/Editor/GUIHierarchy.cs
+++
@@ -1,8 +0,0 @@
-using UnityEditor;
-using UnityEngine;
-using System.Collections;
-
-public class GUIHierarchy
-{
-	//public override bool ShouldDisplay(GameObject obj) { return obj.layer == LayerMask.NameToLayer("UI"); }
-}
