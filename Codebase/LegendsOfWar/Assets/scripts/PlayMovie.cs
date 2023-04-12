3778132cb90b9b4343baf4c8be22619b7e6d4a14
Xander Kyle <xanderclue@gmail.com>
2023-04-12 11:11:22 -0500

    2021.3.22f1

--- Codebase/LegendsOfWar/Assets/scripts/PlayMovie.cs
+++ Codebase/LegendsOfWar/Assets/scripts/PlayMovie.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.Video;
+
 public class PlayMovie : MonoBehaviour
 {
     [SerializeField]
-    private MovieTexture myMovie = null;
+    // private MovieTexture myMovie = null;
+    private VideoPlayer myVideo = null;
     [SerializeField]
     private AudioClip LegendsOfWarMovieBGM = null;
     private void Start()
     {
-        myMovie.Play();
+        // myMovie.Play();
+        myVideo.Play();
         AudioManager.PlaySoundEffect(LegendsOfWarMovieBGM);
     }
     private void OnGUI()
     {
-        GUI.DrawTexture(new Rect(0.0f, 0.0f, Screen.width, Screen.height), myMovie);
+        GUI.DrawTexture(new Rect(0.0f, 0.0f, Screen.width, Screen.height), /*myMovie*/myVideo.texture);
     }
 }
